1782ebdef945f8c4e7212977f583bf8cca9c3cf5
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-03-17 15:13:27 +0530

    Dashboard:Button

--- defi-dashboard/DEFI_DASHBOARD/src/components/Dashboard.jsx
+++ defi-dashboard/DEFI_DASHBOARD/src/components/Dashboard.jsx
@@ -6,10 +6,35 @@
 // Define a functional component that returns JSX for a main section with a heading (e.g., “Welcome to DeFi Dashboard”) and a short paragraph.
 // Export the component.
 
+import { useState } from "react";
+
 
 const DashBoard=()=>{
+    const [view,setView]=useState("List View");
+
+    // function mode(){
+    //     if(view == "List View"){
+    //         setView("Chart View")
+    //     }else if(view == "Chart View"){
+    //         setView("List View")
+    //     }
+    // }
     return(
-        <h2>Welcome to DeFI DashBoard</h2>
+        <div>
+            <h2>Welcome to DeFI DashBoard</h2>
+            <button onClick={()=>{setView(view == "List View" ? "Chart View":"List View")}} style={{color:"green",
+                width:150,
+                height:40,
+                fontFamily:"monospace",
+                fontSize:18,
+                borderRadius:10,
+                background:view == "List View" ? "black":"red"
+                }} > 
+                {view}  
+               
+                </button>
+        </div>
+       
     )
 }
 
