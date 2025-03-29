8abd9a93961460711ca8a6425bf73b105e446ca4
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-03-29 11:44:49 +0530

    Done:Added Header Component

--- task-timeline/client/src/App.jsx
+++ task-timeline/client/src/App.jsx
@@ -1,13 +1,12 @@
-
+import "./App.css";
+import Header from "./Header";
 
 function App() {
-
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="main">
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ task-timeline/client/src/Header.jsx
@@ -0,0 +1,24 @@
+const Header = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "80px",
+        backgroundColor: "#111",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+    >
+      Task-Timeline
+    </div>
+  );
+};
+
+export default Header;
