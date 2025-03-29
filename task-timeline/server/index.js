68c9460b450e2ec1022e389700254b0217ca9b52
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-03-29 09:32:44 +0530

    initial commit

---
+++ task-timeline/server/index.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const app=express();
+require("dotenv").config()
+const port=process.env.PORT;
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello to my world....");
+})
+
+const start=()=>{
+    app.listen(port,()=>{
+    console.log(`server is listening to port ${port}`);
+})
+}
+
+start();
